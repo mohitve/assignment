2db02abf78ba7b478eb061d82f7a3e5e080858b8
Mohit <mohitverma.448@gmail.com>
2018-08-15 23:37:58 +0530

    updated

--- polls/views.py
+++ polls/views.py
@@ -9,7 +9,7 @@ from django.http import HttpResponse
 import requests
 from django.views.decorators.csrf import csrf_exempt
 from django.template.loader import render_to_string
-
+import requests
 
 
 @csrf_exempt
@@ -17,23 +17,26 @@ def index(request):
     if request.method == "GET":
         return render_to_response("polls.html")
     else:
+        print "inside post"
         trans = request.POST.get('tr_code')
-        count = 0
         from urllib3 import HTTPConnectionPool
         from googletrans import Translator
-        pool = HTTPConnectionPool('cobold.xyz' , maxsize = 1)
-        r = pool.request('GET', '/test/Vodafone_Dump_07-Aug-18.xml',fields = {})
-        xml_data = r.data 
+        #pool = HTTPConnectionPool('cobold.xyz' , maxsize = 1)
+        #r = pool.request('GET', '/test/Vodafone_Dump_07-Aug-18.xml',fields = {})
+        #xml_data = r.data
+        r = requests.get(url = "http://cobold.xyz/test/Vodafone_Dump_07-Aug-18.xml")
+	xml_data = r._content
+        print "data received"
 	import xml.etree.ElementTree as ET
 	root = ET.fromstring(xml_data)
         for i in root.findall('Tariff'):
-   	    text = (i.find('Plan_Desc').text)
-            r = i.find('Plan_Desc')
+   	    #i.find('Plan_Desc').text = "MOHIT"
             translator = Translator()
             converted_text = translator.translate(text, dest=str(trans))
             converted = converted_text.extra_data["translation"][0][1]
-            r.set('updated', converted)
- 
+   	    i.find('Plan_Desc').text = converted
+        print root
+    return HttpResponse("I AM FINISHED") 
 
 
 
